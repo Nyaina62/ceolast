5db6198bd0d486fce2a492c4fa241233af244528
DonnellSon <rannelleson@gmail.com>
2024-05-21 17:14:15 +0300

    ff

---
+++ public/assets/js/doslider.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.reportage-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        dots:true
+    });
+});
